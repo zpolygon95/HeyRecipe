c040e7cd7812d2cb2b887a4bcfad307b30cbc3b4
alc48423 <48731134+alc48423@users.noreply.github.com>
2019-03-24 12:01:19 -0400

    Add files via upload

---
+++ FavoriteRecipesActivity.java
@@ -0,0 +1,13 @@
+package io.zjp.heyrecipe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FavoriteRecipesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite_recipes);
+    }
+}
--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,30 @@
 package io.zjp.heyrecipe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRecipesActivity();
+            }
+        });
+    }
+
+    public void openRecipesActivity(){
+        Intent intent = new Intent(this, RecipesActivity.class);
+        startActivity(intent);
     }
 }
