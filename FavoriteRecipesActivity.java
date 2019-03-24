5fa238a69a2694267f625e1990df9583d3415099
alc48423 <48731134+alc48423@users.noreply.github.com>
2019-03-24 17:12:02 -0400

    Add files via upload

--- FavoriteRecipesActivity.java
+++ FavoriteRecipesActivity.java
@@ -2,12 +2,44 @@ package io.zjp.heyrecipe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class FavoriteRecipesActivity extends AppCompatActivity {
+
+    private ListView mainListView ;
+    private ArrayAdapter<String> listAdapter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite_recipes);
+
+        // Find the ListView resource.
+        mainListView = (ListView) findViewById( R.id.mainListView );
+
+        // Create and populate a List of recipe names.
+        String[] recipes = new String[] { "Tacos", "Mac and Cheese", "BBQ Chicken Poppers", "Chinese Chicken Fried Rice",
+                "Chicken Pot Pie"};
+        ArrayList<String> recipeList = new ArrayList<>();
+        recipeList.addAll( Arrays.asList(recipes) );
+
+        // Create ArrayAdapter using the recipe list.
+        listAdapter = new ArrayAdapter<>(this, R.layout.simplerow, recipeList);
+
+        // Add more recipes. If you passed a String[] instead of a List<String>
+        // into the ArrayAdapter constructor, you must not add more items.
+        // Otherwise an exception will occur.
+        listAdapter.add( "Smoky Chicken Chili" );
+        listAdapter.add( "Standing Rib Roast" );
+        listAdapter.add( "Corned Beef and Cabbage" );
+        listAdapter.add( "Beef Burritos" );
+        listAdapter.add( "Cilantro Lime Grilled Shrimp" );
+
+        // Set the ArrayAdapter as the ListView's adapter.
+        mainListView.setAdapter( listAdapter );
+
     }
 }
--- RecipesActivity.java
+++ RecipesActivity.java
@@ -1,13 +1,29 @@
 package io.zjp.heyrecipe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RecipesActivity extends AppCompatActivity {
-
+    private Button button6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes);
+
+        button6 = findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFavoriteRecipesActivity();
+            }
+        });
+    }
+
+    public void openFavoriteRecipesActivity(){
+        Intent intent = new Intent(this, FavoriteRecipesActivity.class);
+        startActivity(intent);
     }
 }
