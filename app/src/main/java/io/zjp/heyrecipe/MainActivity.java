8738f8efaabbc8122e07e7e0f40b61383d7369b3
Aaron Coy <aaronc@umflint.edu>
2019-03-26 14:14:20 -0400

    test

--- app/src/main/java/io/zjp/heyrecipe/MainActivity.java
+++ app/src/main/java/io/zjp/heyrecipe/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // sdfghdfgh
+
         button2 = findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
